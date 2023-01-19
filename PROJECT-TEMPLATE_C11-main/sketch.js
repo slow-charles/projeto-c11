8fa890be40914f24df756eba26a933a4ff8cfbee
slow-charles <115585837+slow-charles@users.noreply.github.com>
2023-01-19 16:07:40 -0300

    Add files via upload

---
+++ PROJECT-TEMPLATE_C11-main/sketch.js
@@ -0,0 +1,28 @@
+var sea,ship,seaImg,shipImg1
+
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-1.png","ship-2.png","ship-1.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+ sea=createSprite(400,200);
+ sea.addImage(seaImg);
+ sea.velocityx = -5;
+ sea.scale=0.3;
+
+ ship = createSprite(130,200,30,30);
+ ship.addAnimation("movingShip",shipImg1);
+ ship.scale = 0.25;
+
+}
+
+function draw() {
+  background("blue");
+
+  sea.velocityx = -3;
+
+    drawSprites();
+ 
+}
